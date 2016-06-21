efd0c7df26b5678eb32fedb40ea46a01cdc9cd07
zhangyuk01 <zhangyuk01@163.com>
2016-06-21 20:52:45 +0800

    初始化项目

---
+++ dianying_app/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dianying_app
+//
+//  Created by 张瑜 on 16/6/21.
+//  Copyright (c) 2016年 张瑜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dianying_app/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dianying_app
+//
+//  Created by 张瑜 on 16/6/21.
+//  Copyright (c) 2016年 张瑜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
